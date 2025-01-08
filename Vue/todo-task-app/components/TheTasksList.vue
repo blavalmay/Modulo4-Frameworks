7e6432a0c9c8c985a79e7aaad2082f699bb9f733
Blanca Valero <bvalero@paycomet.com>
2025-01-08 16:59:47 +0100

    delete single and all tasks

--- Vue/todo-task-app/components/TheTasksList.vue
+++ Vue/todo-task-app/components/TheTasksList.vue
@@ -1,12 +1,35 @@
 <template>
     <h2 class="mt-6">Task List:</h2>
     <div class="p-3 pb-1 mt-3 rounded border">
-        <p v-if="taskList.tasksList.length === 0" class="font-light mb-2">The task list is empty.</p>
+        <p 
+        v-if="taskList.tasksList.length === 0" class="font-light mb-2">The task list is empty.</p>
         <ul>
-            <li class="flex mb-2" v-for="task in taskList.tasksList">
-                <input type="checkbox" title="Check if completed, uncheck if pending" class="mr-2 cursor-pointer">
-                <span class="block grow font-light">{{ task }}</span>
-                <button class="text-red-600 hover:text-red-400" aria-label="Delete task">Delete</button>
+            <li v-if="taskList.tasksList.length > 0" class="flex justify-between mb-2">
+                <input
+                    type="checkbox"
+                    title="Check to complete all tasks"
+                    class="mr-2 cursor-pointer"
+                >
+                <button
+                    class="text-red-600 hover:text-red-400"
+                    aria-label="Delete all tasks"
+                    @click="taskList.removeAllTasks"
+                >Delete all</button>
+            </li>
+            <hr v-if="taskList.tasksList.length > 0" class="mb-2">
+            <li class="flex mb-2" v-for="task in taskList.tasksList" :key="task.id">
+                <input
+                    type="checkbox"
+                    :checked="task.completed"
+                    title="Check if completed, uncheck if pending"
+                    class="mr-2 cursor-pointer"
+                >
+                <span class="block grow font-light">{{ task.description }}</span>
+                <button
+                    class="text-red-600 hover:text-red-400"
+                    aria-label="Delete task"
+                    @click="taskList.removeTask(task.id)"
+                >Delete</button>
             </li>
         </ul>
     </div>
@@ -14,5 +37,4 @@
 
 <script setup lang="ts">
     const taskList = useTaskList();
-    console.log('taskList', taskList.tasksList.length);
 </script>
--- Vue/todo-task-app/composables/useTaskList.ts
+++ Vue/todo-task-app/composables/useTaskList.ts
@@ -1,19 +1,33 @@
 import { defineStore } from "pinia";
 
+type TheTask = {
+    description: string;
+    id: string;
+    completed: boolean;
+}
+
 export const useTaskList = defineStore('taskList', () => {
 
-    const tasksList = ref<string[]>([]);
+    const tasksList = ref<TheTask[]>([]);
 
-    const addNewTask = (task: string) => tasksList.value.push(task);
+    const addNewTask = (task: string) => {
+        const taskId = Math.random().toString(36).substring(2, 15);
+        return tasksList.value.push({description: task, id: taskId, completed: false});
+    }
 
-    const generateTaskId = () => {
-        return Math.random().toString(36).substring(2, 15);
-    };
+    const removeTask = (taskId: string) => {
+        return tasksList.value.splice(tasksList.value.findIndex((task: TheTask) => task.id === taskId), 1);
+    }
+
+    const removeAllTasks = () => {
+        return tasksList.value.splice(0, tasksList.value.length);
+    }
 
     return {
         tasksList,
         addNewTask,
-        generateTaskId
+        removeTask,
+        removeAllTasks,
     };
 }, {
     persist: true,
