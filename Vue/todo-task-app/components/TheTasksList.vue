3ada3706870ad442714cee6342e2955502d95c0f
Blanca Valero <bvalero@paycomet.com>
2025-01-16 13:38:48 +0100

    show completed/pending tasks buttons

--- Vue/todo-task-app/components/TheTasksList.vue
+++ Vue/todo-task-app/components/TheTasksList.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="flex justify-between flex-wrap gap-2 items-end mt-6">
-        <h2>TASK LIST</h2>
+        <h2 class="grow">TASK LIST</h2>
+        <button
+            title="Show only completed tasks"
+            :class="{ 'bg-indigo-600': showCompleted, 'bg-indigo-400': !showCompleted }"
+            @click="toggleShowCompleted"
+            class="px-3 h-8 text-white rounded"
+        >Completed</button>
+        <button
+            title="Show only pending tasks"
+            :class="{ 'bg-indigo-600': showPending, 'bg-indigo-400': !showPending }"
+            @click="toggleShowPending"
+            class="px-3 h-8 text-white rounded"
+        >Pending</button>
         <input 
             type="text"
             class="border rounded px-2 py-1 font-light max-w-full"
@@ -16,7 +28,7 @@
                 <input
                     type="checkbox"
                     :checked="taskList.allTasksCheckbox"
-                    title="Check to complete all tasks"
+                    title="Check to complete/uncomplete all tasks"
                     class="cursor-pointer"
                     @change="(event) => taskList.checkAllTasks((<HTMLInputElement>event.target).checked)"
                 >
@@ -51,14 +63,41 @@
     const taskList = useTaskList();
 
     const taskFilter = ref('');
+    const showCompleted = ref(false);
+    const showPending = ref(false);
 
-    const filteredTasks = computed(() => {
+    const filteredTasks = computed(() => { // la propiedad computed sirve para que la función solo se ejecute cuando cambian sus dependencias (taskList.tasksList y taskFilter.value)
         if(!taskList) {
             return [];
         }
 
         return taskList.tasksList.filter((task: TheTask) => {
-            return task.description.toLowerCase().includes(taskFilter.value.toLowerCase())
+            const filteredTasks = task.description.toLowerCase().includes(taskFilter.value.toLowerCase());
+
+            if (showCompleted.value) {
+                return filteredTasks && task.completed;
+            }
+            if (showPending.value) {
+                return filteredTasks && !task.completed;
+            }
+            return filteredTasks;
         });
     });
+
+    const toggleShowCompleted = () => {
+        if (showCompleted.value) {
+            showCompleted.value = false;
+        } else {
+            showCompleted.value = true;
+            showPending.value = false;
+        }
+    };
+    const toggleShowPending = () => {
+        if (showPending.value) {
+            showPending.value = false;
+        } else {
+            showPending.value = true;
+            showCompleted.value = false;
+        }
+    };
 </script>
